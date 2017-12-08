6b8eed79917bd695ca76484442d2976f1e85fcd0
Yuriy <daomonah@gmail.com>
2017-12-08 15:49:18 +0200

    Create MainActivity and corresponding layout

---
+++ app/src/main/java/com/yuriy_pochynok/remind_me/remindmeandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yuriy_pochynok.remind_me.remindmeandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
